05fa8136af0dbdb161b1f035e7094f8e78de62d9
dekobii <jacobbelmore@gmail.com>
2018-04-20 23:23:50 -0500

    Rhythm + recorder set up

---
+++ Assets/Jacob's Toybox/Scripts/Recorder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recorder : MonoBehaviour {
+    float Timer = 0;
+    bool mouseClick;
+    bool spaced;
+    List<float> times = new List<float>();
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Timer += Time.deltaTime;
+        mouseClick = Input.GetMouseButtonDown(0);
+        spaced = Input.GetKeyDown(KeyCode.Space);
+        if (mouseClick) {
+            times.Add(Timer);
+            Debug.Log(Timer);
+        }
+        if (spaced) {
+            printArray(times);
+        }
+
+	}
+    public void printArray(List<float> a)
+    {
+        string stringToOut = "0, ";
+        foreach (var i in a)
+        {
+            stringToOut += i.ToString("0.0000") + ", ";
+        }
+        Debug.Log(stringToOut);
+    }
+}
