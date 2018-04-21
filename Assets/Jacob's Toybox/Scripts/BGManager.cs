063e20e9499bd26f26ef20836f4170c3621129c4
dekobii <jacobbelmore@gmail.com>
2018-04-21 12:34:16 -0500

    song and ui updates

--- Assets/Jacob's Toybox/Scripts/BGManager.cs
+++ Assets/Jacob's Toybox/Scripts/BGManager.cs
@@ -11,11 +11,11 @@ public class BGManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Time.time > 102)
+        if (Time.time > 52)
         {
             Material m = gameObject.GetComponent<Renderer>().material;
             Color c = m.color;
-            m.SetColor("_Color", c * ((106 - Time.time) / 4));
+            m.SetColor("_Color", c * ((58 - Time.time) / 8));
         }
     }
 }
