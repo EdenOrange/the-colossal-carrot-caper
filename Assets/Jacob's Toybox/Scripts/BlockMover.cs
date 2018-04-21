5431adbb26aa4b1e8e1495e382b6bd1cd6f9826b
dekobii <jacobbelmore@gmail.com>
2018-04-20 23:37:25 -0500

    blocks moving

---
+++ Assets/Jacob's Toybox/Scripts/BlockMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockMover : MonoBehaviour {
+    float speed = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+    }
+
+    public void SetStartingPosition(float x) {
+        gameObject.transform.position += new Vector3(x, 0, 0);
+    }
+}
