9a7376816ead473e278c4888d80ffeee7221cd46
dekobii <jacobbelmore@gmail.com>
2018-04-22 00:56:32 -0500

    Wall Remover Script

---
+++ Assets/Jacob's Toybox/Scripts/SunLightScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunLightScript : MonoBehaviour {
+    public GameObject dl;
+    public float RotationSpeed = 2.0f;
+    private float progress = 0f;
+    Light lightComp;
+
+    // Use this for initialization
+    void Start () {
+        lightComp = dl.GetComponent<Light>();
+        lightComp.type = LightType.Directional;
+        dl.transform.position = new Vector3(0, 0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        progress += RotationSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.up * (RotationSpeed * Time.deltaTime));
+        if (progress < 10 || progress > 40)
+        {
+            lightComp.color = Color.blue;
+
+        }
+        else if (progress < 20 || progress > 30)
+        {
+            lightComp.color = Color.yellow; 
+
+        }
+        else {
+            lightComp.color = Color.white; 
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/WallPrefabHiderScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallPrefabHiderScript : MonoBehaviour {
+    GameObject[] walls;
+	// Use this for initialization
+	void Start () {
+        walls = GameObject.FindGameObjectsWithTag("Wall");
+
+        foreach (GameObject wall in walls)
+        {
+            wall.GetComponent<Renderer>().enabled = false;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
